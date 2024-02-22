f7d43478fd14f8c6450477c29a29421f76bd4ef0
jdbertron <jdbertron@gmail.com>
2024-02-21 23:11:56 -0700

    Added Diamond test.

--- test/Diamond.ts
+++ test/Diamond.ts
@@ -5,6 +5,8 @@ import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
 import { expect } from 'chai';
 import {BqETH} from '../types/contracts/index.ts';
+import {MerkleTreeVerifier,PietrzakVerifier,LibBqETH} from '../types/contracts/libraries/index.ts';
+import {BqETHDecrypt, BqETHPublish, BqETHSolve, IERC20MetaDataStub} from '../types/contracts/facets/index.ts';
 
 const REWARDPERDAY = 700;
 const SECPER32EXP = 145300;
@@ -13,6 +15,9 @@ describe("BqETH contract", function () {
   var diamondProxyAddress: string;
 
   beforeEach(async () => {
+      // await hre.network.provider.send('evm_setAutomine', [false]);
+      // await hre.network.provider.send('evm_setIntervalMining', [100]);
+
       await hre.deployments.fixture(["BqETHDiamond"]);
       // console.log(`Diamond Deployment finished`);
       const {owner} = await hre.getNamedAccounts();
@@ -25,6 +30,16 @@ describe("BqETH contract", function () {
       // console.log(`Wrote BqETH Facet setRewardPerDay`);
       await BqETHFacet.setSecondsPer32Exp(145300).then((tx) => tx.wait()); 
       // console.log(`Wrote BqETH Facet setSecondsPer32Exp`);
+  });
+
+  it("Deployment should have the 3 important facets", async function () {
+    // 'BigNumbers' is the only one missing because the typechain isn't generated
+    const BqETHDecrypt = (await hre.ethers.getContractAt('BqETHDecrypt', diamondProxyAddress)) as BqETHDecrypt;
+    expect(BqETHDecrypt).not.undefined;
+    const BqETHPublish = (await hre.ethers.getContractAt('BqETHPublish', diamondProxyAddress)) as BqETHPublish;
+    expect(BqETHPublish).not.undefined;
+    const BqETHSolve = (await hre.ethers.getContractAt('BqETHSolve', diamondProxyAddress)) as BqETHSolve;
+    expect(BqETHSolve).not.undefined;
   });
 
   it("Deployment should have the right version", async function () {
